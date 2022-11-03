3a1632203401d2cf0c6454120477c492971d9da5
Juan Sanchez <juan.sanchez@juanleonardosanchez.com>
2022-11-03 13:05:46 -0400

    update reverser to be within module.exports

--- handler.js
+++ handler.js
@@ -1,11 +1,11 @@
 // write your lambda here
 
-export default function reverser(input) {
+module.exports.reverser = (input) => {
   let output = ""
   for(let i = input.length - 1; i >= 0; i--){
     output = output + input[i]
   }
-  
+
   return {
     "statusCode": 200,
     "body": {
