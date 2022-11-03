26351a11340bb6e7ff823207772b221f7b46f733
Juan Sanchez <juan.sanchez@juanleonardosanchez.com>
2022-11-03 13:13:40 -0400

    add logging

--- handler.js
+++ handler.js
@@ -1,11 +1,15 @@
 // write your lambda here
 
 module.exports.reverser = (input) => {
+  console.log("Reverser::begin")
   let output = ""
+
+  console.log("Input", input)
   for(let i = input.length - 1; i >= 0; i--){
     output = output + input[i]
   }
-
+  
+  console.log("Output", output)
   return {
     "statusCode": 200,
     "body": {
