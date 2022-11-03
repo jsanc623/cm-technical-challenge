180d4d3e5b70d4525729fdbae557b20760dc4450
Juan Sanchez <juan.sanchez@juanleonardosanchez.com>
2022-11-03 12:48:14 -0400

    add reverser handler

--- handler.js
+++ handler.js
@@ -1,1 +1,16 @@
 // write your lambda here
+
+function reverser(input) {
+  let output = ""
+  for(let i = input.length - 1; i >= 0; i--){
+    output = output + input[i]
+  }
+  
+  return {
+    "statusCode": 200,
+    "body": {
+      "message": "Thank you for using the reverser!",
+      "payload": output
+    }
+  }
+}
